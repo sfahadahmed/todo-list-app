e531c0ed0ae50bdb36e62303f638c9a359374eb7
sfahadahmed <sfahadahmed@gmail.com>
2018-02-21 17:06:55 +0500

    Adding initial support with barebones project structure.

---
+++ src/main/java/com/fahad/todolistapp/TodoListAppApplication.java
@@ -0,0 +1,12 @@
+package com.fahad.todolistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListAppApplication.class, args);
+	}
+}
