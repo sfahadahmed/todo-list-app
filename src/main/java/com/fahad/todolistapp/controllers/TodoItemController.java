374d58237ed48a776dc28a392be3d4cd962303d8
sfahadahmed <sfahadahmed@gmail.com>
2018-02-21 22:43:42 +0500

    - Adding support for CORS request while development. - Bug Fix: forgot to return list of TodoItems for the default handler

--- src/main/java/com/fahad/todolistapp/controllers/TodoItemController.java
+++ src/main/java/com/fahad/todolistapp/controllers/TodoItemController.java
@@ -3,6 +3,7 @@ package com.fahad.todolistapp.controllers;
 import com.fahad.todolistapp.models.TodoItem;
 import com.fahad.todolistapp.repositories.TodoItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,17 +16,20 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api/todoitems")
+@CrossOrigin(origins = "http://localhost:4200")
 public class TodoItemController {
 
     @Autowired
     private TodoItemRepository todoItemRepository;
 
     @GetMapping()
-    public void getAll(){
+    public List<TodoItem> getAll(){
         List<TodoItem> todoItems = new ArrayList<TodoItem>();
         for(TodoItem todoItem : todoItemRepository.findAll()){
             todoItems.add(todoItem);
         }
+
+        return todoItems;
     }
 
     @GetMapping("/by-id/{id}")
