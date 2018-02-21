e0c117675ed76bc2581f9b0322f81ac416a3365d
sfahadahmed <sfahadahmed@gmail.com>
2018-02-21 22:44:31 +0500

    Adding support to create some mock data when starting up the application.

--- src/main/java/com/fahad/todolistapp/TodoListAppApplication.java
+++ src/main/java/com/fahad/todolistapp/TodoListAppApplication.java
@@ -1,12 +1,32 @@
 package com.fahad.todolistapp;
 
+import com.fahad.todolistapp.models.TodoItem;
+import com.fahad.todolistapp.repositories.TodoItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoListAppApplication {
+public class TodoListAppApplication implements CommandLineRunner {
+
+    @Autowired
+    private TodoItemRepository todoItemRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListAppApplication.class, args);
 	}
+
+    @Override
+    public void run(String... strings) throws Exception {
+        todoItemRepository.deleteAll();
+
+        todoItemRepository.save(new TodoItem("Task 1", "This is task 1"));
+        todoItemRepository.save(new TodoItem("Task 2", "This is task 2"));
+        todoItemRepository.save(new TodoItem("Task 3", "This is task 3"));
+
+        for(TodoItem todoItem : todoItemRepository.findAll()){
+            System.out.println(todoItem);
+        }
+    }
 }
