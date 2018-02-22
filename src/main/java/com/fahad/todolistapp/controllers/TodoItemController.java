fc43f2dcbd257a05253726211611e56fccd76737
sfahadahmed <sfahadahmed@gmail.com>
2018-02-22 22:40:33 +0500

    Adding support for complete CRUD RESTful operations.

--- src/main/java/com/fahad/todolistapp/controllers/TodoItemController.java
+++ src/main/java/com/fahad/todolistapp/controllers/TodoItemController.java
@@ -3,10 +3,14 @@ package com.fahad.todolistapp.controllers;
 import com.fahad.todolistapp.models.TodoItem;
 import com.fahad.todolistapp.repositories.TodoItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Fahad Ahmed
@@ -31,11 +35,36 @@ public class TodoItemController {
 
     @GetMapping("/{id}")
     public TodoItem getById(@PathVariable long id){
-        return todoItemRepository.findById(id);
+        Optional<TodoItem> todoItem = Optional.ofNullable(todoItemRepository.findById(id));
+        return todoItem.get();
     }
 
-    @GetMapping("/by-title/{id}")
-    public TodoItem getByTitle(@PathVariable String title){
-        return todoItemRepository.findByTitle(title);
+    @PostMapping
+    public ResponseEntity<Object> create(@RequestBody TodoItem todoItem){
+        TodoItem savedTodoItem = todoItemRepository.save(todoItem);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+            .buildAndExpand(savedTodoItem.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(@PathVariable long id, @RequestBody TodoItem todoItem){
+        Optional<TodoItem> savedTodoItem = Optional.ofNullable(todoItemRepository.findById(id));
+
+        if(!savedTodoItem.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        else {
+            todoItem.setId(id);
+            todoItemRepository.save(todoItem);
+
+            return ResponseEntity.noContent().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id){
+        todoItemRepository.delete(id);
     }
 }
